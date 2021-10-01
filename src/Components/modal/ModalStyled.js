f4a389923532c2f3b580352145a853a123d1dc0e
AndriiShnurkov <Shans12.84@ukr.net>
2021-10-01 19:15:32 +0300

    added styles

--- src/Components/modal/ModalStyled.js
+++ src/Components/modal/ModalStyled.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 
 export const OverLay = styled.div`
   position: fixed;
-  top: 60px;
+  top: 0px;
   left: 0;
   width: 100vw;
-  height: calc(100vh - 60px);
+  height: 100vh;
   z-index: 99999;
   background-color: rgba(0, 0, 0, 0.7);
   display: flex;
